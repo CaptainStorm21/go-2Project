43f94704a15be6c419a6759ca138d1b61be55acc
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-12-20 17:36:24 -0800

    update

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 	"log"
 	"os"
 	"strings"
+	"flag"
 )
 
 func main() {
 
-	f, err := os.Open("myapp.log")
+	path := flag.String("path", "myapp.log", "the path to the log that should be analyzed")
+	level := flag.String("level", "ERROR", "log level search for OPTIONS : DEBUG, INFO, ERROR, CRITICAL")
+
+	flag.Parse()
+
+	f, err := os.Open(*path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,7 +27,7 @@ func main() {
 		if err != nil {
 			break
 		}
-		if strings.Contains(s, "ERROR") {
+		if strings.Contains(s, *level) {
 			fmt.Println(s)
 		}
 	}
